38b7f7c5b46d7e3185ab98c43a167ce27dd3a7ba
CosmicSand <141958407+CosmicSand@users.noreply.github.com>
2023-12-30 21:06:01 +0000

    Deploying to gh-pages from @ CosmicSand/goit-js-hw-11@d2d8a57adb7fc66b41f48063af68a0c1f095b484 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,10 +1,9 @@
-import{i as f,S as m}from"./assets/vendor-46aac873.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))t(e);new MutationObserver(e=>{for(const s of e)if(s.type==="childList")for(const r of s.addedNodes)r.tagName==="LINK"&&r.rel==="modulepreload"&&t(r)}).observe(document,{childList:!0,subtree:!0});function n(e){const s={};return e.integrity&&(s.integrity=e.integrity),e.referrerpolicy&&(s.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?s.credentials="include":e.crossorigin==="anonymous"?s.credentials="omit":s.credentials="same-origin",s}function t(e){if(e.ep)return;e.ep=!0;const s=n(e);fetch(e.href,s)}})();const h="/goit-js-hw-11/assets/error-cc3a3fd7.svg",l=document.querySelector(".searching-form");l.addEventListener("submit",g);function g(i){i.preventDefault();const o=i.currentTarget.elements.searching.value,n=new URLSearchParams({key:"41547319-253ef689baf3db6007ef0d5b5",q:`${o}`,orientation:"horizontal",per_page:9,image_type:"photo",safesearch:!0});fetch(`https://pixabay.com/api/?${n}`).then(t=>t.json()).then(t=>{if(t.hits.length===0)throw f.error({title:"Error",message:"Sorry, there are no images matching your search query. Please try again!",iconUrl:`${h}`,backgroundColor:"#EF4040",titleColor:"#fff",messageColor:"#fff",messageSize:"16px",progressBarColor:"#B5EA7C",position:"topRight"}),new Error("Error fetching data");y(t)}).catch(t=>console.log(t))}function y(i){const o=document.querySelector(".gallery");o.innerHTML="",i.hits.map((n,t)=>{const e=i.hits[t].largeImageURL,s=i.hits[t].webformatURL,r=i.hits[t].tags,c=i.hits[t].likes,a=i.hits[t].views,p=i.hits[t].comments,d=i.hits[t].downloads,u=`<li class="gallery-item"><div class='image-wrapper'>
+import{i as g,S as u}from"./assets/vendor-46aac873.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))s(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const o of t.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&s(o)}).observe(document,{childList:!0,subtree:!0});function l(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerpolicy&&(t.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?t.credentials="include":e.crossorigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function s(e){if(e.ep)return;e.ep=!0;const t=l(e);fetch(e.href,t)}})();const f="/goit-js-hw-11/assets/error-cc3a3fd7.svg",a=document.querySelector(".searching-form");a.addEventListener("submit",h);function h(i){i.preventDefault();const r=i.currentTarget.elements.searching.value,l=new URLSearchParams({key:"41547319-253ef689baf3db6007ef0d5b5",q:`${r}`,orientation:"horizontal",per_page:9,image_type:"photo",safesearch:!0});fetch(`https://pixabay.com/api/?${l}`).then(s=>s.json()).then(s=>{if(s.hits.length===0)throw g.error({title:"Error",message:"Sorry, there are no images matching your search query. Please try again!",iconUrl:`${f}`,backgroundColor:"#EF4040",titleColor:"#fff",messageColor:"#fff",messageSize:"16px",progressBarColor:"#B5EA7C",position:"topRight"}),new Error("Error fetching data");y(s)}).catch(s=>console.log(s))}function y(i){const r=document.querySelector(".gallery");r.innerHTML="",i.hits.map((l,s)=>{const{largeImageURL:e,webformatURL:t,tags:o,likes:n,views:c,comments:p,downloads:d}=i.hits[s],m=`<li class="gallery-item"><div class='image-wrapper'>
   <a class="gallery-link" href="${e}">
     <img
       class="gallery-image"
-      src="${s}"
-      data-source="${e}"
-      alt="${r}"
+      src="${t}"
+      alt="${o}"
       width="360"
       height="200"
     />
@@ -13,11 +12,11 @@ import{i as f,S as m}from"./assets/vendor-46aac873.js";(function(){const o=docum
 <ul class='gallery-item-description-list'>
 <li class='description-list-item'>
 <p class='description'>Likes</p>
-<p class='quantity'>${c}</p>
+<p class='quantity'>${n}</p>
 </li>
 <li class='description-list-item'>
 <p class='description'>Views</p>
-<p class='quantity'>${a}</p>
+<p class='quantity'>${c}</p>
 </li>
 <li class='description-list-item'>
 <p class='description'>Comments</p>
@@ -30,5 +29,5 @@ import{i as f,S as m}from"./assets/vendor-46aac873.js";(function(){const o=docum
 </ul>
     </div>
     </div>
-</li>`;o.insertAdjacentHTML("beforeend",u)}),new m(".gallery a",{captionsData:"alt",captionDelay:250,className:"lightbox-on"}),l.reset()}
+</li>`;r.insertAdjacentHTML("beforeend",m)}),new u(".gallery a",{captionsData:"alt",captionDelay:250,className:"lightbox-on"}),a.reset()}
 //# sourceMappingURL=commonHelpers.js.map
