264d22ec385a05b4fe6a19fd91ec8f2a4aeb48b7
CosmicSand <141958407+CosmicSand@users.noreply.github.com>
2023-12-30 20:40:46 +0000

    Deploying to gh-pages from @ CosmicSand/goit-js-hw-11@ca7e02e81c1db92f4022cbb3fb39a9577a1a8929 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,4 +1,4 @@
-import{i as u,S as f}from"./assets/vendor-46aac873.js";(function(){const i=document.createElement("link").relList;if(i&&i.supports&&i.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))t(e);new MutationObserver(e=>{for(const s of e)if(s.type==="childList")for(const r of s.addedNodes)r.tagName==="LINK"&&r.rel==="modulepreload"&&t(r)}).observe(document,{childList:!0,subtree:!0});function l(e){const s={};return e.integrity&&(s.integrity=e.integrity),e.referrerpolicy&&(s.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?s.credentials="include":e.crossorigin==="anonymous"?s.credentials="omit":s.credentials="same-origin",s}function t(e){if(e.ep)return;e.ep=!0;const s=l(e);fetch(e.href,s)}})();const m="/goit-js-hw-11/assets/error-cc3a3fd7.svg",h=document.querySelector(".searching-form");h.addEventListener("submit",g);function g(o){o.preventDefault();const i=o.currentTarget.elements.searching.value,l=new URLSearchParams({key:"41547319-253ef689baf3db6007ef0d5b5",q:`${i}`,orientation:"horizontal",per_page:9,image_type:"photo",safesearch:!0});fetch(`https://pixabay.com/api/?${l}`).then(t=>t.json()).then(t=>{if(t.hits.length===0)throw u.error({title:"Error",message:"Sorry, there are no images matching your search query. Please try again!",iconUrl:`${m}`,backgroundColor:"#EF4040",titleColor:"#fff",messageColor:"#fff",messageSize:"16px",progressBarColor:"#B5EA7C",position:"topRight"}),new Error("Error fetching data");y(t)}).catch(t=>console.log(t))}function y(o){const i=document.querySelector(".gallery");o.hits.map((l,t)=>{const e=o.hits[t].largeImageURL,s=o.hits[t].webformatURL,r=o.hits[t].tags,n=o.hits[t].likes,c=o.hits[t].views,a=o.hits[t].comments,p=o.hits[t].downloads,d=`<li class="gallery-item"><div class='image-wrapper'>
+import{i as f,S as m}from"./assets/vendor-46aac873.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))t(e);new MutationObserver(e=>{for(const s of e)if(s.type==="childList")for(const r of s.addedNodes)r.tagName==="LINK"&&r.rel==="modulepreload"&&t(r)}).observe(document,{childList:!0,subtree:!0});function n(e){const s={};return e.integrity&&(s.integrity=e.integrity),e.referrerpolicy&&(s.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?s.credentials="include":e.crossorigin==="anonymous"?s.credentials="omit":s.credentials="same-origin",s}function t(e){if(e.ep)return;e.ep=!0;const s=n(e);fetch(e.href,s)}})();const h="/goit-js-hw-11/assets/error-cc3a3fd7.svg",l=document.querySelector(".searching-form");l.addEventListener("submit",g);function g(i){i.preventDefault();const o=i.currentTarget.elements.searching.value,n=new URLSearchParams({key:"41547319-253ef689baf3db6007ef0d5b5",q:`${o}`,orientation:"horizontal",per_page:9,image_type:"photo",safesearch:!0});fetch(`https://pixabay.com/api/?${n}`).then(t=>t.json()).then(t=>{if(t.hits.length===0)throw f.error({title:"Error",message:"Sorry, there are no images matching your search query. Please try again!",iconUrl:`${h}`,backgroundColor:"#EF4040",titleColor:"#fff",messageColor:"#fff",messageSize:"16px",progressBarColor:"#B5EA7C",position:"topRight"}),new Error("Error fetching data");y(t)}).catch(t=>console.log(t))}function y(i){const o=document.querySelector(".gallery");o.innerHTML="",i.hits.map((n,t)=>{const e=i.hits[t].largeImageURL,s=i.hits[t].webformatURL,r=i.hits[t].tags,c=i.hits[t].likes,a=i.hits[t].views,p=i.hits[t].comments,d=i.hits[t].downloads,u=`<li class="gallery-item"><div class='image-wrapper'>
   <a class="gallery-link" href="${e}">
     <img
       class="gallery-image"
@@ -13,22 +13,22 @@ import{i as u,S as f}from"./assets/vendor-46aac873.js";(function(){const i=docum
 <ul class='gallery-item-description-list'>
 <li class='description-list-item'>
 <p class='description'>Likes</p>
-<p class='quantity'>${n}</p>
-</li>
-<li class='description-list-item'>
-<p class='description'>Views</p>
 <p class='quantity'>${c}</p>
 </li>
 <li class='description-list-item'>
-<p class='description'>Comments</p>
+<p class='description'>Views</p>
 <p class='quantity'>${a}</p>
 </li>
 <li class='description-list-item'>
-<p class='description'>Downloads</p>
+<p class='description'>Comments</p>
 <p class='quantity'>${p}</p>
+</li>
+<li class='description-list-item'>
+<p class='description'>Downloads</p>
+<p class='quantity'>${d}</p>
 </li>
 </ul>
     </div>
     </div>
-</li>`;i.insertAdjacentHTML("beforeend",d)}),new f(".gallery a",{captionsData:"alt",captionDelay:250,className:"lightbox-on"})}
+</li>`;o.insertAdjacentHTML("beforeend",u)}),new m(".gallery a",{captionsData:"alt",captionDelay:250,className:"lightbox-on"}),l.reset()}
 //# sourceMappingURL=commonHelpers.js.map
